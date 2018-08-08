fc4b55f32f2ac45ca6d81ee9a66efc99d46f18a0
maevet78 <190439mt@saintviator.com>
2018-08-08 15:22:13 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,31 +1,31 @@
 STUDENTS NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga", "Nicki Minaj", "Rihanna"]
 
 def first_student_by_index
-  puts student_names[0]
+  puts STUDENT NAMES[0]
 end
 first_student_by_index
 
-def fourth_student_by_index
-  puts student_names[3]
-end
-fourth_student_by_index
+# def fourth_student_by_index
+#   puts student_names[3]
+# end
+# fourth_student_by_index
 
-def last_student_by_index
-  puts student_names[5]
-end
-last_student_by_index
+# def last_student_by_index
+#   puts student_names[5]
+# end
+# last_student_by_index
 
-def first_student_by_method
-  puts student_names.first
-end
-first_student_by_method
+# def first_student_by_method
+#   puts student_names.first
+# end
+# first_student_by_method
 
-def last_student_by_method
-  puts student_names.last
-end
-last_student_by_method
+# def last_student_by_method
+#   puts student_names.last
+# end
+# last_student_by_method
 
-def first_second_and_third_students
-  puts student_names[1..3]
-end
-first_second_and_third_students
+# def first_second_and_third_students
+#   puts student_names[1..3]
+# end
+# first_second_and_third_students
