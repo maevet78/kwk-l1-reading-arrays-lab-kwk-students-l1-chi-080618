6d1e35b8332d271d85a66a60e9f5fc426cc17242
maevet78 <190439mt@saintviator.com>
2018-08-08 15:21:54 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,4 @@
-student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga", "Nicki Minaj", "Rihanna"]
+STUDENTS NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga", "Nicki Minaj", "Rihanna"]
 
 def first_student_by_index
   puts student_names[0]
